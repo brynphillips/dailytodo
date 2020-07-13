d0b067ae4bd47ec362590a2cfd497651552e98cc
brynphillips <brynphillips1@gmail.com>
2020-07-12 17:20:37 -0700

    added user options

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@
 
 def printTodoList(inputlist: list) -> type(None):
     for x in inputlist:
-        print(x + "\n")
+        print(f'{inputlist.index(x) + 1}. {x}\n')
 
 def addTodoList() -> str:
     answer = input('What do you want to add to your todo list?\n')
@@ -12,25 +12,40 @@ def delTodoList() -> str:
     answer = input('What do you want to delete to your todo list?\n')
     return answer
 
-def main():
-    todolist = []
-
+def initask():
     answer = input(
     """What would you like to add to your todo list?
+    0. View list?
     1. Add to the todo list?
     2. Delete from todo list?
     3. Quit?\n"""
     )
+    return answer
 
+def main():
+    todolist = []
+#initask
 
-    if answer == "1":
-        added = addTodoList()
-        todolist.append(added)
+    while True:
+        answer = initask()
 
-    if answer == "2":
-        deleted = delTodoList()
+# answers
+        if answer == '0':
+            printTodoList(todolist)
 
-    printTodoList(todolist)
+        if answer == '1':
+            added = addTodoList()
+            todolist.append(added)
+
+        if answer == '2':
+            deleted = delTodoList()
+            for x in todolist:
+                if x.tolower() == deleted.tolower():
+                    todolist.pop(index(x))
+                    continue
+
+        if answer == '3':
+            return False
 
 if __name__ == '__main__':
     exit(main())
